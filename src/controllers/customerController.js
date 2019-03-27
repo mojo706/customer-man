84a49e57a6f3b3fbc4ffeff9b0c7d8db03ebc019
basedHokage <eugene.omar@andela.com>
2019-03-27 10:48:34 +0300

    Add methods to retrieve customer information by id, phone name or just all customers

--- src/controllers/customerController.js
+++ src/controllers/customerController.js
@@ -1,13 +1,13 @@
 const { Customers } = require('../models')
 
 const addCustomer = async (req, res) => {
-  const [newUser, created] = await Customers.findOrCreate({
+  const [newCustomer, created] = await Customers.findOrCreate({
     where: req.body,
     defaults: req.body,
     plain: true,
   })
   if (created) {
-    res.status(201).json(newUser)
+    res.status(201).json(newCustomer)
     return
   }
   res.status(409).json({
@@ -15,18 +15,55 @@ const addCustomer = async (req, res) => {
   })
 }
 
+const getAllCustomers = async (req, res) => {
+  const customers = await Customers.findAll()
+  res.status(200).json(customers)
+}
+
+const findByID = async (req, res) => {
+  const { customer } = res.locals
+  res.status(200).json(customer)
+}
+
+const findByNameOrPhone = async (req, res) => {
+  let found
+  const params = req.query
+  if (!params.name && !params.phone) {
+    return res.status(400).json({
+      message: 'Please enter valid search params',
+    })
+  }
+  if (params.name && !params.phone) {
+    found = await Customers.findOne({
+      where: { name: params.name },
+    })
+    return res.status(200).json(found)
+  }
+  found = await Customers.findOne({
+    where: { phone: params.phone },
+  })
+  return res.status(200).json(found)
+}
+
 const updateCustomer = async (req, res) => {
-  const user = await Customers.findById(req.params.userId)
-  await user.update(req.body)
-  res.status(200).json(user)
+  const { customer } = res.locals
+  await customer.update(req.body)
+  res.status(200).json(customer)
 }
 
 const deleteCustomer = async (req, res) => {
-  const user = await Customers.findById(req.params.userId)
-  await user.destroy()
+  const { customer } = res.locals
+  await customer.destroy()
   res.status(200).json({
     message: 'That user has been deleted',
   })
 }
 
-module.exports = { addCustomer, updateCustomer, deleteCustomer }
+module.exports = {
+  addCustomer,
+  getAllCustomers,
+  findByID,
+  findByNameOrPhone,
+  updateCustomer,
+  deleteCustomer,
+}
